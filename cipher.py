20ad38587ce24f8a1424a5dbab4543fb74e040ca
Aliyah Gant <aliyahgant5@gmail.com>
2024-04-07 17:05:37 -0400

    Caesar Cipher Completed! :)

--- cipher.py
+++ cipher.py
@@ -1,1 +1,17 @@
-# add your code here
+#Aliyah Gant's Caesar Cipher
+sentence = input("Please enter a sentence: ")
+sentence = sentence.lower()
+new_sent = ""
+alpha = "abcdefghijklmnopqrstuvwxyzabcde"
+
+
+for s in sentence:
+    for a in alpha:
+        letter = a
+        if a == s:
+            switch = alpha.index(a) + 5
+            new_sent += alpha[switch]
+            break
+    if letter != s:
+        new_sent += s
+print("The encrypted sentence is: " + new_sent)# add your code here
